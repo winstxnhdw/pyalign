6a09623fb86bba879a0e11e65e28bd7a68c370d8
winstxnhdw <winstxnhdw@gmail.com>
2023-05-30 08:30:38 +0800

    chore: initialise repository

---
+++ main.py
@@ -0,0 +1,23 @@
+from numpy import array, vectorize
+from numpy.core.defchararray import str_len
+from pyscript import Element
+
+
+def align():
+    """
+    Summary
+    -------
+    aligns the text in the input field by the character specified in the single-char-field
+    """
+    align_character: str | None = Element('single-char-field').value
+    input_text: str | None = Element('text-field').value
+
+    if not align_character or not input_text:
+        print('Please fill in all fields!')
+        return
+
+    input_matrix = array([line.split(align_character) for line in input_text.split('\n')])  # type: ignore
+    length_of_columns = str_len(input_matrix.T).max(axis=1)
+    formatter = vectorize(lambda cell, length: f'{cell:{length}}')
+    formatted_matrix = formatter(input_matrix, length_of_columns)
+    Element('output-field').write('\n'.join('|'.join(row) for row in formatted_matrix))
