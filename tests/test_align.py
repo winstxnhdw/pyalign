d56a5b219c0b8d0b6de3be7f15d8e2eba55690ef
winstxnhdw <winstxnhdw@gmail.com>
2025-03-17 20:52:36 +0000

    test: add specific `ruff` noqa

--- tests/test_align.py
+++ tests/test_align.py
@@ -1,9 +1,9 @@
-# ruff: noqa
+# ruff: noqa: S101
 
 from main import align
 
 
-def test_align():
+def test_align() -> None:
     poorly_aligned_string = """| Action| Command|
 | -------| -------|
 | Hello | `World` |
--- tests/test_pad_cell.py
+++ tests/test_pad_cell.py
@@ -1,9 +1,9 @@
-# ruff: noqa
+# ruff: noqa: S101
 
 from main import pad_cell
 
 
-def test_align():
+def test_align() -> None:
     assert pad_cell("Hello", 0) == "Hello"
     assert pad_cell("Hello", 5) == "Hello"
     assert pad_cell("Hello", 6) == "Hello "
